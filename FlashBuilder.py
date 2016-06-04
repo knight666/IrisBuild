ce051607af1b2fcef3d60b5650f5f7bffed6e821
Quinten Lansu <quintenlansu@gmail.com>
2016-06-04 15:46:25 +0200

    Refactor publish settings class into a separate library.

--- FlashBuilder.py
+++ FlashBuilder.py
@@ -1,39 +1,6 @@
-import xml.etree.ElementTree as ET
-import libs.flash.XMLUtils as fxml
-
-class PublishSettings:
-	def __init__(self):
-		pass
-
-	def parse(self, filename):
-		root = ET.parse(filename).getroot()
-
-		profile = root.find('flash_profile')
-		if not profile:
-			raise SyntaxError('Missing "flash_profile" element in hierarchy.')
-
-		publish_format = profile.find('PublishFormatProperties')
-		if not publish_format:
-			raise SyntaxError('Missing "PublishFormatProperties" element under "flash_profile" element.')
-
-		self.enabled = publish_format.attrib['enabled'] == 'true'
-		self.publishPath = fxml.readElement(publish_format, 'flashFileName')
-
-		publish_flash = profile.find('PublishFlashProperties')
-		if not publish_flash:
-			raise SyntaxError('Missing "PublishFlashProperties" element under "flash_profile" element.')
-
-		self.versionActionScript = fxml.readElement(publish_flash, 'ActionScriptVersion')
-		if self.versionActionScript == '3':
-			self.packagePaths = fxml.readPathList(fxml.readElement(publish_flash, 'AS3PackagePaths'))
-			self.constants = fxml.readPathList(fxml.readElement(publish_flash, 'AS3ConfigConst'))
-		else:
-			self.packagePaths = fxml.readPathList(fxml.readElement(publish_flash, 'PackagePaths'))
-			self.constants = []
-
-		self.documentClass = fxml.readElement(publish_flash, 'DocumentClass')
+import libs.flash.Settings as fs 
 
 if __name__ == '__main__':
-	publish = PublishSettings()
+	publish = fs.Publish()
 	publish.parse('projects/TestEmpty/PublishSettings.xml')
 	print('enabled', publish.enabled, 'publishPath', publish.publishPath, 'versionActionScript', publish.versionActionScript, 'packagePaths', publish.packagePaths, 'constants', publish.constants, 'documentClass', publish.documentClass)
---
+++ libs/flash/Settings.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+import libs.flash.XMLUtils as fxml
+
+class Publish:
+	def __init__(self):
+		pass
+
+	def parse(self, filename):
+		root = ET.parse(filename).getroot()
+
+		profile = root.find('flash_profile')
+		if not profile:
+			raise SyntaxError('Missing "flash_profile" element in hierarchy.')
+
+		publish_format = profile.find('PublishFormatProperties')
+		if not publish_format:
+			raise SyntaxError('Missing "PublishFormatProperties" element under "flash_profile" element.')
+
+		self.enabled = publish_format.attrib['enabled'] == 'true'
+		self.publishPath = fxml.readElement(publish_format, 'flashFileName')
+
+		publish_flash = profile.find('PublishFlashProperties')
+		if not publish_flash:
+			raise SyntaxError('Missing "PublishFlashProperties" element under "flash_profile" element.')
+
+		self.versionActionScript = fxml.readElement(publish_flash, 'ActionScriptVersion')
+		if self.versionActionScript == '3':
+			self.packagePaths = fxml.readPathList(fxml.readElement(publish_flash, 'AS3PackagePaths'))
+			self.constants = fxml.readPathList(fxml.readElement(publish_flash, 'AS3ConfigConst'))
+		else:
+			self.packagePaths = fxml.readPathList(fxml.readElement(publish_flash, 'PackagePaths'))
+			self.constants = []
+
+		self.documentClass = fxml.readElement(publish_flash, 'DocumentClass')
