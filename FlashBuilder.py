13c20970546ec800e58b86dbae43df5f8606e6e9
Quinten Lansu <quintenlansu@gmail.com>
2016-06-04 15:08:39 +0200

    Refactor parsing of publish settings as a class.

--- FlashBuilder.py
+++ FlashBuilder.py
@@ -1,11 +1,45 @@
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('projects/TestEmpty/PublishSettings.xml')
-root = tree.getroot()
-profile = root.find('flash_profile')
-publish_settings = profile.find('PublishFormatProperties')
-flash_fileName = publish_settings.find('flashFileName')
+class PublishSettings:
+	def __init__(self):
+		pass
 
-print('PublishFormatProperties')
-print('enabled', publish_settings.attrib['enabled'])
-print('flashFileName', flash_fileName.text)
+	def parse(self, filename):
+		root = ET.parse(filename).getroot()
+
+		profile = root.find('flash_profile')
+		if not profile:
+			raise SyntaxError('Missing "flash_profile" element in hierarchy.')
+
+		publish_format = profile.find('PublishFormatProperties')
+		if not publish_format:
+			raise SyntaxError('Missing "PublishFormatProperties" element under "flash_profile" element.')
+
+		self.enabled = publish_format.attrib['enabled'] == 'true'
+		self.publishPath = self.readElement(publish_format, 'flashFileName')
+
+		publish_flash = profile.find('PublishFlashProperties')
+		if not publish_flash:
+			raise SyntaxError('Missing "PublishFlashProperties" element under "flash_profile" element.')
+
+		self.versionActionScript = self.readElement(publish_flash, 'ActionScriptVersion')
+		if self.versionActionScript == '3':
+			self.packagePaths = self.readElement(publish_flash, 'AS3PackagePaths')
+			self.constants = self.readElement(publish_flash, 'AS3ConfigConst')
+		else:
+			self.packagePaths = self.readElement(publish_flash, 'PackagePaths')
+			self.constants = []
+
+		self.documentClass = self.readElement(publish_flash, 'DocumentClass')
+
+	def readElement(self, parent, name):
+		element = parent.find(name)
+		if element == None:
+			raise SyntaxError('Missing "' + str(name) + '" element under "' + str(parent.tag) + '" element.')
+
+		return element.text
+
+if __name__ == '__main__':
+	publish = PublishSettings()
+	publish.parse('projects/TestEmpty/PublishSettings.xml')
+	print('enabled', publish.enabled, 'publishPath', publish.publishPath, 'versionActionScript', publish.versionActionScript, 'packagePaths', publish.packagePaths, 'constants', publish.constants, 'documentClass', publish.documentClass)
