9b707cab086240596afa1274c845f05e03343949
Quinten Lansu <quintenlansu@gmail.com>
2016-06-05 21:56:37 +0200

    Clean up installation API.

--- install.py
+++ install.py
@@ -2,35 +2,27 @@ from glob import glob
 import os
 import shutil
 
+def installFile(fromPath, toPath):
+	print('Copying "' + fromPath + '"...')
+
+	if not os.path.exists(os.path.dirname(toPath)):
+		os.makedirs(os.path.dirname(toPath))
+
+	shutil.copy(os.path.abspath(fromPath), toPath)
+
 if __name__ == '__main__':
 	# TODO: Check for multiple installation paths
-	target = os.path.abspath(os.getenv('LOCALAPPDATA') + '\\Adobe\\Flash CS5.5\\en_US\\Configuration\\')
-	target_commands = target + '\\Commands'
+	target = os.path.abspath(os.getenv('LOCALAPPDATA') + '\\Adobe\\Flash CS5.5\\en_US\\Configuration\\Commands')
 
 	print('Installing at "' + target + '".')
 
-	if not os.path.exists(target_commands):
-		os.makedirs(target_commands)
+	if not os.path.exists(target):
+		os.makedirs(target)
 
 	for f in glob('scripts\\commands\\*.jsfl'):
-		from_path = os.path.abspath(f)
-		to_path = os.path.abspath(target_commands + '\\' + os.path.basename(f))
+		installFile(f, os.path.abspath(target + '\\' + os.path.basename(f)))
 
-		print('Copying "' + os.path.basename(f) + '"...')
+	installFile('FlashBuilder.py', target)
 
-		shutil.copy(from_path, to_path)
-
-	print('Copying "FlashBuilder.py"...')
-
-	shutil.copy(os.path.abspath('FlashBuilder.py'), target_commands)
-
-	for f in glob('libs/**/*.py', recursive=True):
-		from_path = os.path.abspath(f)
-		to_path = target_commands + '\\' + f
-
-		if not os.path.exists(os.path.dirname(to_path)):
-			os.makedirs(os.path.dirname(to_path))
-
-		print('Copying "' + f + '"...')
-
-		shutil.copy(os.path.abspath(f), to_path)
+	for f in glob('libs\\**\\*.py', recursive=True):
+		installFile(f, target + '\\' + f)
