e721990997ab2069dd03cc1db9e716d9e7c4e39a
Quinten Lansu <quintenlansu@gmail.com>
2016-06-05 21:31:25 +0200

    Installer copies commands to Flash commands folder.

--- install.py
+++ install.py
@@ -1,5 +1,19 @@
+from glob import glob
 import os
+import shutil
 
 if __name__ == '__main__':
-	target = os.path.abspath(os.getenv('LOCALAPPDATA') + '\\Adobe\\Flash CC\\language\\Configuration\\')
-	print(target)
+	# TODO: Check for multiple installation paths
+	target = os.path.abspath(os.getenv('LOCALAPPDATA') + '\\Adobe\\Flash CS5.5\\en_US\\Configuration\\')
+	target_commands = target + '\\Commands'
+
+	print('Installing scripts at "' + target + '".')
+
+	if not os.path.exists(target_commands):
+		os.makedirs(target_commands)
+
+	for f in glob('scripts\\commands\\*.jsfl'):
+		print('Copying "' + os.path.basename(f) + '"...')
+		from_path = os.path.abspath(f)
+		to_path = os.path.abspath(target_commands + '\\' + os.path.basename(f))
+		shutil.copy(from_path, to_path)
