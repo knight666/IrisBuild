016ade08252a1f4ce340fb72460e5795c3de2f58
Quinten Lansu <Q.Lansu@bigpoint.net>
2016-06-08 10:35:32 +0200

    Allow installation for multiple Flash versions.

--- install.py
+++ install.py
@@ -12,24 +12,33 @@ def installFile(fromPath, toPath):
 	shutil.copy(os.path.abspath(fromPath), toPath)
 
 if __name__ == '__main__':
-	# TODO: Check for multiple installation paths
-	target = os.path.abspath(os.getenv('LOCALAPPDATA') + '\\Adobe\\Flash CS5.5\\en_US\\Configuration\\Commands')
+	languages = [ 'en_US' ]
+	versions = [ 'Flash CS5.5', 'Flash CC 2015', 'Animate CC 2015' ]
+	userpath = os.getenv('LOCALAPPDATA') + '\\Adobe\\'
 
-	print('Installing at "' + target + '".')
+	for v in versions:
+		for l in languages:
+			install = os.path.join(userpath, v, l)
+			if not os.path.exists(install):
+				continue
 
-	if not os.path.exists(target):
-		os.makedirs(target)
+			target = os.path.join(install, 'Configuration', 'Commands')
 
-	if os.path.exists('output\\windows\\Win32\\Release\\IrisBuild.dll'):
-		installFile('output\\windows\\Win32\\Release\\IrisBuild.dll', target)
-	elif os.path.exists('output\\windows\\Win32\\Debug\\IrisBuild.dll'):
-		installFile('output\\windows\\Win32\\Debug\\IrisBuild.dll', target)
+			print('Installing at "' + target + '".')
 
-	for f in glob('scripts\\commands\\*.jsfl'):
-		installFile(f, os.path.abspath(target + '\\' + os.path.basename(f)))
+			if not os.path.exists(target):
+				os.makedirs(target)
 
-	installFile('FlashBuilder.py', target)
+			if os.path.exists('output\\windows\\Win32\\Release\\IrisBuild.dll'):
+				installFile('output\\windows\\Win32\\Release\\IrisBuild.dll', target)
+			elif os.path.exists('output\\windows\\Win32\\Debug\\IrisBuild.dll'):
+				installFile('output\\windows\\Win32\\Debug\\IrisBuild.dll', target)
 
-	for root, dirnames, filenames in os.walk('libs'):
-		for filename in fnmatch.filter(filenames, '*.py'):
-			installFile(os.path.join(root, filename), os.path.join(target, 'libs', *dirnames) + '\\' + filename)
+			for f in glob('scripts\\commands\\*.jsfl'):
+				installFile(f, os.path.abspath(target + '\\' + os.path.basename(f)))
+
+			installFile('FlashBuilder.py', target)
+
+			for root, dirnames, filenames in os.walk('libs'):
+				for filename in fnmatch.filter(filenames, '*.py'):
+					installFile(os.path.join(root, filename), os.path.join(target, 'libs', *dirnames) + '\\' + filename)
