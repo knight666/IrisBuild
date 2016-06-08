a994966beb3296c4f2b837972965cde43d8725d5
Quinten Lansu <Q.Lansu@bigpoint.net>
2016-06-08 10:38:28 +0200

    Install shared libraries in the correct folder.

--- install.py
+++ install.py
@@ -22,23 +22,30 @@ if __name__ == '__main__':
 			if not os.path.exists(install):
 				continue
 
-			target = os.path.join(install, 'Configuration', 'Commands')
+			print('Installing at "' + install + '".')
 
-			print('Installing at "' + target + '".')
+			target_commands = os.path.join(install, 'Configuration', 'Commands')
+			if not os.path.exists(target_commands):
+				os.makedirs(target_commands)
 
-			if not os.path.exists(target):
-				os.makedirs(target)
+			target_libs = os.path.join(install, 'Configuration', 'External Libraries')
+			if not os.path.exists(target_libs):
+				os.makedirs(target_libs)
+
+			# Shared library
 
 			if os.path.exists('output\\windows\\Win32\\Release\\IrisBuild.dll'):
-				installFile('output\\windows\\Win32\\Release\\IrisBuild.dll', target)
+				installFile('output\\windows\\Win32\\Release\\IrisBuild.dll', target_libs)
 			elif os.path.exists('output\\windows\\Win32\\Debug\\IrisBuild.dll'):
-				installFile('output\\windows\\Win32\\Debug\\IrisBuild.dll', target)
+				installFile('output\\windows\\Win32\\Debug\\IrisBuild.dll', target_libs)
+
+			# Scripts
 
 			for f in glob('scripts\\commands\\*.jsfl'):
-				installFile(f, os.path.abspath(target + '\\' + os.path.basename(f)))
+				installFile(f, os.path.abspath(target_commands + '\\' + os.path.basename(f)))
 
-			installFile('FlashBuilder.py', target)
+			installFile('FlashBuilder.py', target_commands)
 
 			for root, dirnames, filenames in os.walk('libs'):
 				for filename in fnmatch.filter(filenames, '*.py'):
-					installFile(os.path.join(root, filename), os.path.join(target, 'libs', *dirnames) + '\\' + filename)
+					installFile(os.path.join(root, filename), os.path.join(target_commands, 'libs', *dirnames) + '\\' + filename)
